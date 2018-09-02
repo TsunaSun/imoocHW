e2a1a63faffc8ec239f6a4f2cd419ef5db415b73
Tsuna <735068990@qq.com>
2018-09-02 22:37:05 +0800

    第三次作业：多态

---
+++ ImoocHomework3/src/com/model/IAct.java
@@ -0,0 +1,8 @@
+package com.model;
+
+public interface IAct {
+	//抽象方法：描述技能、描述表演
+	void skill();
+	
+	void act();
+}
---
+++ ImoocHomework3/src/com/model/Parrot.java
@@ -0,0 +1,48 @@
+package com.model;
+
+public class Parrot extends Animal implements IAct{
+	//新增属性：种类
+	private String type;
+	
+	//get set
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public void skill() {
+		// TODO Auto-generated method stub
+		System.out.println("技能：会算算术");
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		System.out.println("表演者：" + this.getName());
+		System.out.println("年龄：" + this.getAge());
+		System.out.println("种类：" + this.getType());
+		this.skill();
+
+	}
+
+	@Override
+	public void love() {
+		// TODO Auto-generated method stub
+		System.out.println("爱好：吃桃子吃香蕉");
+	}
+
+	public Parrot() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Parrot(String name, int age, String type) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+		this.setType(type);
+	}
+}
